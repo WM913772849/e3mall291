c20f6aeb95c7f491ccd6939c47c34aeef41131e4
WangMing <wangming_0114@163.com>
2018-02-01 13:15:34 +0800

    e3mall-manager

---
+++ e3mall-manager/e3mall-manager-service/src/main/java/cn/e3mall/manager/service/impl/ItemServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.e3mall.manager.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.manager.service.ItemService;
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	public TbItem findItemById(Long itemId) {
+		// TODO Auto-generated method stub
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		return item;
+	}
+
+}
